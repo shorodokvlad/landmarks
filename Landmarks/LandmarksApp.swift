4dd15e30680e1fb2d2fc321390dfc82e20c2f3e5
Vladislav Shorodok <vlad.shorodok14@gmail.com>
2025-10-06 11:44:54 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vlad Shorodok on 06.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
