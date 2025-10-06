b3f96ec296494f86806bd276c8a2f28e3161a1e3
Vladislav Shorodok <vladislav.shorodok@student.usv.ro>
2025-10-06 12:33:38 +0300

    Add landmarkData

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,31 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Vlad Shorodok on 06.10.2025.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
