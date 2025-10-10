c07cfba6909b40c9e76b301d487d597ce9227d41
Vladislav Shorodok <vladislav.shorodok@student.usv.ro>
2025-10-10 23:46:32 +0300

    Add user profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Vlad Shorodok on 10.10.2025.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20 ) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
